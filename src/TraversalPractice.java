97595ac52667cbf0476d2815b84e988ac6521788
DevRudy <55453823+SketchRudy@users.noreply.github.com>
2025-05-05 21:46:07 -0700

    Finished some methods

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -8,6 +8,15 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   public static void printOddNodes(Node<Integer> node) {
+    if (node == null) {
+      return;
+    }
+
+    printOddNodes(node.left);
+    printOddNodes(node.right);
+    if (node.value % 2 != 0) {
+      System.out.println(node.value);
+    }
 
   }
 
@@ -20,6 +29,15 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   public static <T> void printNodesWithOneChild(Node<T> node) {
+    if (node == null) {
+      return;
+    }
+
+    if (node.left == null || node.right == null) {
+      System.out.println(node.value);
+    }
+    printNodesWithOneChild(node.left);
+    printNodesWithOneChild(node.right);
     
   }
 
@@ -32,6 +50,11 @@ public class TraversalPractice {
    * @return the sum 
    */
   public static int treeSum(Node<Integer> node) {
+    if (node == null) {
+      return 0;
+    }
+
+    
     return 0;
   }
 
